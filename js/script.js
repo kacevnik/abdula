87d701c961de9896fbb34108cb4b4b3895b884db
Dmitriy Kovalev Home <kodimweb@gmail.com>
2019-10-26 00:08:44 +0300

    finish section profit

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ jQuery( document ).ready( function( $ ) {
         if($(window).width() >= 768){
             const img_video = $('.prez_video_img');
             $('.prez_video').css({'left': img_video.width()/2 - 50});
+        } else {
+            $('.prez_video').css({'left': ($('.prez_video').parents('.row.position-relative').width() - $('.prez_video').width())/2});
         }
     }
 
