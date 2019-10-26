9e2ff5660c3512d80180721a626184933100e624
Dmitriy Kovalev Home <kodimweb@gmail.com>
2019-10-26 17:27:03 +0300

    added sectiom format

--- js/script.js
+++ js/script.js
@@ -15,4 +15,30 @@ jQuery( document ).ready( function( $ ) {
 
     imgVideo();
 
+    var swiper = new Swiper('.format_swiper', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+    });
+
+    swiper.on('transitionEnd', function () {
+        $('.format_slide_content').html($('.swiper-slide-active .format_slide_text').html());
+    });
+
+    $("a[href*='#']").bind("click", function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 500);
+		e.preventDefault();
+		return false;
+	});
+
 });
