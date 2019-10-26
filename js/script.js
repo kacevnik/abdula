6a8b428883ea29f5da8d63b784b9471de0474be9
Dmitriy Kovalev Home <kodimweb@gmail.com>
2019-10-26 18:41:15 +0300

    added section party

--- js/script.js
+++ js/script.js
@@ -28,6 +28,36 @@ jQuery( document ).ready( function( $ ) {
         },
     });
 
+    var party_swiper = new Swiper('.party-slider', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination-party',
+        },
+        navigation: {
+          nextEl: '.party-swiper-button-next',
+          prevEl: '.party-swiper-button-prev',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20
+            },
+            // when window width is >= 480px
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 30
+            },
+
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+          }
+    });
+
     swiper.on('transitionEnd', function () {
         $('.format_slide_content').html($('.swiper-slide-active .format_slide_text').html());
     });
